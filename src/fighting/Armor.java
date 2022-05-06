61349f246c7fc7150944523a3e208bd40bcbb9e1
CR0YD <98400750+CR0YD@users.noreply.github.com>
2022-05-06 19:23:26 +0200

    Kommentierung

--- src/fighting/Armor.java
+++ src/fighting/Armor.java
@@ -2,14 +2,14 @@ package fighting;
 
 public class Armor {
 
-	public final int DEF;
+	public final int DEF; // defense
 	public final String NAME;
 	public final ArmorType TYPE;
 
 	public static enum ArmorType {
 		Leather, Iron
 	}
-	
+
 	public Armor(String name, int def, ArmorType type) {
 		NAME = name;
 		DEF = def;
