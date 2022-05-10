0a27b3bbac9ee251659b6b71e8b73a2801e1bc5f
gdjhd32 <wilgawilken@gmail.com>
2022-05-10 20:51:08 +0200

    Kleine Weiterentwicklunge
    An dem CombatAutomaten weiter gearbeitet und überlegt ein hilfsprogramm
    zur Erstellung der Automaten.

---
+++ src/automatCreator/AutomatCreator.java
@@ -0,0 +1,16 @@
+package automatCreator;
+
+import javax.swing.JFrame;
+
+public class AutomatCreator extends JFrame {
+	
+	public static void main(String[] args) {
+		new AutomatCreator();
+	}
+	
+	public AutomatCreator() {
+		setBounds(500, 500, 500, 500);
+		setVisible(true);
+	}
+	
+}
--- src/fighting/CombatAutomat.java
+++ src/fighting/CombatAutomat.java
@@ -1,39 +1,75 @@
 package fighting;
 
 public class CombatAutomat {
-	
+
 	CombatSituation[] situations;
 	CombatAction[][] matrix;
-	
+
 	public CombatAutomat() {
 		tempCreateAutomat();
 	}
-	
+
+	// temporary
 	private void tempCreateAutomat() {
 		situations = new CombatSituation[2];
-		
+		situations[0] = new CombatSituation("Du stehst dem Monster gegenüber.");
+		situations[1] = new CombatSituation(null);
+
 		matrix = new CombatAction[situations.length][situations.length];
-//		matrix[0][1] = new CombatAction();
-		
+		matrix[0][1] = new CombatAction(new String[] { "k" }, null, null, null,
+				"Du stichst dein Schwert in die Richtung des Monsters.");
+		matrix[0][1] = new CombatAction(null, null, new String[] {" "}, new int[] { 1 },
+				"");
+
 	}
-	
+
 	private class CombatAction {
-		
+
+		String description;
 		String[] keys;
 		int[] timesForKeys;
-		
-		public CombatAction(String[] keys, int[] timesForKeys) {
-			keys = keys;
-			timesForKeys = timesForKeys;
+
+		String[] cKeys;
+		int[] cTimesForKeys;
+
+		/**
+		 * @param keys
+		 * @param timesForKeys All values in seconds.
+		 */
+		/**
+		 * c = computer, meaning the actions the computer can take.
+		 * 
+		 * @param keys
+		 * @param timesForKeys  Time in seconds.
+		 * @param cKeys
+		 * @param cTimesForKeys Time in seconds.
+		 * @param description
+		 */
+		public CombatAction(String[] keys, int[] timesForKeys, String[] cKeys, int[] cTimesForKeys,
+				String description) {
+			this.keys = keys;
+			this.timesForKeys = timesForKeys;
+			this.description = description;
+			this.cKeys = cKeys;
 		}
-		
+
+		public String[] getKeys() {
+			return keys;
+		}
+
+		public int[] getTimesForKeys() {
+			return timesForKeys;
+		}
+
 	}
-	
+
 	private class CombatSituation {
-		
-		public CombatSituation() {
-			
+
+		String description;
+
+		public CombatSituation(String description) {
+			this.description = description;
 		}
-		
+
 	}
 }
