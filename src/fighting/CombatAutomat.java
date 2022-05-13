9741dec9ae5ec434fa4c3ee545787871b7f0b41a
gdjhd32 <wilgawilken@gmail.com>
2022-05-13 19:00:16 +0200

    Kleine Veränderungen
    Importieren der notwendigen Libaries für das bearbeiten und einlesen von
    xmls Dateien und kleine Umstrukturierung der früheren Adjazenzmatrix.

--- src/fighting/CombatAutomat.java
+++ src/fighting/CombatAutomat.java
@@ -1,9 +1,10 @@
 package fighting;
 
+import org.apache.poi.xssf.usermodel.*;
+
 public class CombatAutomat {
 
 	CombatSituation[] situations;
-	CombatAction[][] matrix;
 
 	public CombatAutomat() {
 		tempCreateAutomat();
@@ -14,56 +15,15 @@ public class CombatAutomat {
 		situations = new CombatSituation[2];
 		situations[0] = new CombatSituation("Du stehst dem Monster gegenüber.");
 		situations[1] = new CombatSituation(null);
-
-		matrix = new CombatAction[situations.length][situations.length];
-		matrix[0][1] = new CombatAction(new String[] { "k" }, null, null, null,
-				"Du stichst dein Schwert in die Richtung des Monsters.");
-		matrix[0][1] = new CombatAction(null, null, new String[] {" "}, new int[] { 1 },
-				"");
-
-	}
-
-	private class CombatAction {
-
-		String description;
-		String[] keys;
-		int[] timesForKeys;
-
-		String[] cKeys;
-		int[] cTimesForKeys;
-
-		/**
-		 * @param keys
-		 * @param timesForKeys All values in seconds.
-		 */
-		/**
-		 * c = computer, meaning the actions the computer can take.
-		 * 
-		 * @param keys
-		 * @param timesForKeys  Time in seconds.
-		 * @param cKeys
-		 * @param cTimesForKeys Time in seconds.
-		 * @param description
-		 */
-		public CombatAction(String[] keys, int[] timesForKeys, String[] cKeys, int[] cTimesForKeys,
-				String description) {
-			this.keys = keys;
-			this.timesForKeys = timesForKeys;
-			this.description = description;
-			this.cKeys = cKeys;
-		}
-
-		public String[] getKeys() {
-			return keys;
-		}
-
-		public int[] getTimesForKeys() {
-			return timesForKeys;
-		}
+		
+		
+		XSSFWorkbook s = new XSSFWorkbook();
+		XSSFSheet g = s.getSheetAt(0);
 
 	}
 
 	private class CombatSituation {
+		//String[] keys, int[] timesForKeys, String[] cKeys, int[] cTimesForKeys, String description
 
 		String description;
 
